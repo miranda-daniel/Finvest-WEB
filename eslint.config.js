724bcdcc3727d99d181733a624f65353b50d451c
miranda-daniel <miranda_daniel@hotmail.com>
2026-04-23 19:23:20 -0300

    chore: upgrade to Node 24, update all dependencies, migrate to ESLint 10

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import reactPlugin from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
 import prettierConfig from 'eslint-config-prettier'
 
@@ -12,19 +11,14 @@ export default tseslint.config(
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
-      react: reactPlugin,
       'react-hooks': reactHooksPlugin,
     },
     languageOptions: {
       globals: globals.browser,
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
       ...reactHooksPlugin.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off', // Not needed with React 17+ JSX transform
-    },
-    settings: {
-      react: { version: 'detect' },
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
   prettierConfig
